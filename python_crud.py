a936a349ae818acbfa9cf32d619cfeca09cdbe64
karlaguinaldodev <aguinaldokarl.al@gmail.com>
2025-02-12 12:08:22 +0800

    Successful na yung pag connect ng python sa postgresql and pinaganda yung code gamit GPT and trying to understand it even more

--- python_crud.py
+++ python_crud.py
@@ -20,26 +20,76 @@ def read_all_persons(conn):
     cursor.close()
     return persons
 
-def read_person(conn, person_id):
-    pass
+def read_person(conn, person_id): 
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM person WHERE id = %s", (person_id,))
+    person = cursor.fetchone()
+    cursor.close()
+    return person
 
 def update_person(conn, person_id, person):
-    pass
+    cursor = conn.cursor()
+    cursor.execute("UPDATE person SET firstname = %s, lastname = %s, age = %s WHERE id = %s", (person['firstname'], person['lastname'], person['age'], person_id))
+    person = cursor.fetchnon()
+    cursor.close()
+    return person
 
 def delete_person(conn, person_id):
-    pass
-
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM person WHERE id = %s", (person_id,))
+    conn.commit()
+    cursor.close()
 
 if __name__ == "__main__":
     conn = psycopg2.connect(
         host="localhost",
         database="postgres",
-        user="admin",
+        user="gelo",
         password="admin"
     )
 
-    # create_person(conn, "John", "Doe", 30)
-    persons = read_all_persons(conn)
-    print(persons)
 
-    print(persons[0][1])
+#from GPT, pinaganda ko lang yung code and trying to understand it even more hehe. di pa masyado gets super dami ng codes hehe dami pa errors
+while True:
+        print("Select an operation:")
+        print("1 - Create person")
+        print("2 - Read all persons")
+        print("3 - Read person")
+        print("4 - Update person")
+        print("5 - Delete person")
+        print("6 - Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == '1':
+            firstname = input("Enter first name: ")
+            lastname = input("Enter last name: ")
+            age = input("Enter age: ")
+            create_person(conn, firstname, lastname, age)
+        elif choice == '2':
+            persons = read_all_persons(conn)
+            for person in persons:
+                print(person)
+        elif choice == '3':
+            person_id = input("Enter person ID: ")
+            person = read_person(conn, person_id)
+            print(person)
+        elif choice == '4':
+            person_id = input("Enter person ID: ")
+            firstname = input("Enter new first name: ")
+            lastname = input("Enter new last name: ")
+            age = input("Enter new age: ")
+            update_person(conn, person_id, firstname, lastname, age)
+        elif choice == '5':
+            person_id = input("Enter person ID: ")
+            delete_person(conn, person_id)
+        elif choice == '6':
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+        conn.close()
+
+if __name__ == "__main__":
+    main()
+   
